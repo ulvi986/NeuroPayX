96b9e043eeeb3f679022bc47ada126e66040ae28
ulvi986 <64612489+ulvi986@users.noreply.github.com>
2025-11-19 02:53:03 +0400

    Update settings.py

--- neuropayx/settings.py
+++ neuropayx/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gob7#!#!nx7^w293uu^=3(^ue@h!45s=)5qcu=s_&ovw95@4rp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'neuropayx.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # Yerel geliştirme için varsayılan
+        conn_max_age=600,
+    )
 }
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True  # Railway bağlantıları için gerekli olabilir
+    )
 
 
 # Password validation
