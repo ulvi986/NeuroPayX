671fd254b375b96469d3febaa001f6500ca5a6d0
ulvi986 <64612489+ulvi986@users.noreply.github.com>
2025-11-19 03:04:01 +0400

    Update settings.py

--- neuropayx/settings.py
+++ neuropayx/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gob7#!#!nx7^w293uu^=3(^ue@h!45s=)5qcu=s_&ovw95@4rp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
